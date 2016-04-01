0eba0fb0cdeeb0cfa9cc1bcdef95363f96956bc7
shnhrrsn <shnhrrsn@gmail.com>
2016-04-01 15:25:44 -0400

    Removed Host/Port typealiases + added convenience extension to Server to serve on 0.0.0.0

--- Sources/Server.swift
+++ Sources/Server.swift
@@ -1,3 +1,9 @@
 public protocol Server {
-    func serve(responder: Responder, on host: Host, at port: Port) throws
+    func serve(responder: Responder, on host: String, at port: Int) throws
+}
+
+extension Server {
+    public func serve(responder: Responder, at port: Int) throws {
+        try self.serve(responder, on: "0.0.0.0", at: port)
+    }
 }
