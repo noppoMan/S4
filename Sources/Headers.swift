a5e68b3b5f0c637b9227a74a05ddd466755b7344
Paulo Faria <paulo.faria.rl@gmail.com>
2016-05-05 09:56:51 -0300

    use single headers

--- Sources/Headers.swift
+++ Sources/Headers.swift
@@ -1,14 +1,14 @@
 public struct Headers {
-    public var headers: [CaseInsensitiveString: Header]
+    public var headers: [CaseInsensitiveString: String]
 
-    public init(_ headers: [CaseInsensitiveString: Header]) {
+    public init(_ headers: [CaseInsensitiveString: String]) {
         self.headers = headers
     }
 }
 
 extension Headers: DictionaryLiteralConvertible {
-    public init(dictionaryLiteral elements: (CaseInsensitiveString, Header)...) {
-        var headers: [CaseInsensitiveString: Header] = [:]
+    public init(dictionaryLiteral elements: (CaseInsensitiveString, String)...) {
+        var headers: [CaseInsensitiveString: String] = [:]
 
         for (key, value) in elements {
             headers[key] = value
@@ -20,7 +20,7 @@ extension Headers: DictionaryLiteralConvertible {
 
 extension Headers: Sequence {
     #if swift(>=3.0)
-    public func makeIterator() -> DictionaryIterator<CaseInsensitiveString, Header> {
+    public func makeIterator() -> DictionaryIterator<CaseInsensitiveString, String> {
         return headers.makeIterator()
     }
     #else
@@ -37,9 +37,9 @@ extension Headers: Sequence {
         return headers.isEmpty
     }
 
-    public subscript(field: CaseInsensitiveString) -> Header {
+    public subscript(field: CaseInsensitiveString) -> String? {
         get {
-            return headers[field] ?? []
+            return headers[field]
         }
 
         set(header) {
@@ -47,13 +47,28 @@ extension Headers: Sequence {
         }
     }
 
-    public subscript(field: CaseInsensitiveStringRepresentable) -> Header {
+    public subscript(field: CaseInsensitiveStringRepresentable) -> String? {
         get {
-            return headers[field.caseInsensitiveString] ?? []
+            return headers[field.caseInsensitiveString]
         }
 
         set(header) {
             headers[field.caseInsensitiveString] = header
         }
+    }
+}
+
+extension Headers: CustomStringConvertible {
+    public var description: String {
+        var string = ""
+
+        for (header, value) in headers {
+            string += header.string
+            string += ": "
+            string += value
+            string += "\n"
+        }
+
+        return string
     }
 }
