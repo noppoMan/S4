c97edca353e90aa9be620cd35396b5bc1d199bdf
Paulo Faria <paulo.faria.rl@gmail.com>
2016-06-09 16:44:00 -0300

    add cookieHeaders

--- Sources/Response.swift
+++ Sources/Response.swift
@@ -2,13 +2,15 @@ public struct Response: Message {
     public var version: Version
     public var status: Status
     public var headers: Headers
+    public var cookieHeaders: Set<String>
     public var body: Body
     public var storage: [String: Any] = [:]
 
-    public init(version: Version, status: Status, headers: Headers, body: Body) {
+    public init(version: Version, status: Status, headers: Headers, cookieHeaders: Set<String>, body: Body) {
         self.version = version
         self.status = status
         self.headers = headers
+        self.cookieHeaders = cookieHeaders
         self.body = body
     }
 }
